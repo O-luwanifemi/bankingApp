2ab3388b880d0d1e5880956707e2538c80f38727
Nifemi <rashid.oluwaseun@yahoo.com>
2021-11-03 01:10:46 -0700

    Added: Banking app project

---
+++ BankingApp/BankingApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BankingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wema = new Bank();
+            Console.WriteLine(wema.contactUser("Joseph Oloba"));
+        }
+    }
+
+    class Bank
+    {
+        public Bank()
+        {
+
+        }
+
+        public string Name { get; set; }
+        private uint numberOfStaffs { get; set; }
+        public string headOfficeAddress { get; set; }
+
+        public string receiveMoney(string sender, int amountSaved)
+        {
+            string transactionDetails = $"{amountSaved} by {sender}";
+            return transactionDetails;
+        }
+
+        public string sendMoney(string sender, string recipient, int amount)
+        {
+            string transactionDetails = $"{amount} sent by {sender} to {recipient}";
+            return transactionDetails;
+        }
+
+        public string contactUser(string user)
+        {
+            return user;
+        }
+    }
+}
